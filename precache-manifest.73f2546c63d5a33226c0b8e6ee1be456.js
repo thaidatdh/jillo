a054548fc278ea7cc5520d953e4ac1d728920e7e
Dat Ho <thaidatdh@gmail.com>
2020-10-25 07:55:03 +0700

    Updates

---
+++ precache-manifest.73f2546c63d5a33226c0b8e6ee1be456.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "075ce4a8966b135af5c2932cbf724529",
+    "url": "/jillo/index.html"
+  },
+  {
+    "revision": "3b71409ab57ae753955c",
+    "url": "/jillo/static/css/main.ef7805e4.chunk.css"
+  },
+  {
+    "revision": "9785d4130b8364bac047",
+    "url": "/jillo/static/js/2.754bc6ff.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/jillo/static/js/2.754bc6ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3b71409ab57ae753955c",
+    "url": "/jillo/static/js/main.b3dc3eec.chunk.js"
+  },
+  {
+    "revision": "ddba7d215a1b97852807",
+    "url": "/jillo/static/js/runtime-main.8b83d31a.js"
+  }
+]);
